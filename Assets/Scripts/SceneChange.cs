561e0b2abdeadd97b5295da6e91f7eaff59bd47b
Hans (cgt103529) <cgt103529@stud.fh-wedel.de>
2023-05-31 20:54:10 +0200

    Level Update

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -1,18 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private string _nextScene;
+    private void OnTriggerEnter(Collider other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(_nextScene);
     }
 }
