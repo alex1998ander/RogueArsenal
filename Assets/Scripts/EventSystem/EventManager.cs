49af7f257eeea28220674ce55b517ea4af575460
Erik <ejansen127@gmail.com>
2023-06-06 02:08:59 +0200

    Extension of event system

--- Assets/Scripts/EventSystem/EventManager.cs
+++ Assets/Scripts/EventSystem/EventManager.cs
@@ -1,15 +1,47 @@
 using System;
 
-// Event naming convention: On + [contributor] + [action]
+// Event naming convention: On + [contributor] + [action / event]
 // Examples for contributors: Player, Enemy, Weapon
 
 public static class EventManager {
+    #region Player Events
 
     /// <summary>
-    /// Player fires his weapon. This event is triggered regardless of the number of bullets or shots.
+    /// The player fires his weapon (Fire button was pressed). This event is triggered regardless of the number of bullets or shots.
     /// </summary>
     public static readonly Event OnPlayerFire = new();
-    
 
+    /// <summary>
+    /// The player takes damage. The amount of damage is passed as a parameter.
+    /// </summary>
+    public static readonly Event<Damage> OnPlayerDamage = new();
+
+    /// <summary>
+    /// The player dies.
+    /// </summary>
+    public static readonly Event OnPlayerDeath = new();
+
+    #endregion
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    #region Enemy Events
+
+    /// <summary>
+    /// A Enemy takes damage. The amount of damage is passed as a parameter.
+    /// </summary>
+    public static readonly Event<Damage> OnEnemyDamage = new();
+
+    /// <summary>
+    /// The player dies.
+    /// </summary>
+    public static readonly Event<EnemyController> OnEnemyDeath = new();
+
+    #endregion
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    #region Game Events
+
+    #endregion
 }
-
