1d8dcbd7c110043a669eb8558f2f15c8c54f84c3
Erik <ejansen127@gmail.com>
2023-06-06 02:01:40 +0200

    Basic combat system

--- Assets/Characters/AimPlayer/PlayerController.cs
+++ Assets/Characters/AimPlayer/PlayerController.cs
@@ -8,10 +8,10 @@ public class PlayerController : MonoBehaviour, IUpgradeablePlayer {
     private PlayerInput _playerInput;
 
     [SerializeField] private float moveSpeed = 5f;
-    [SerializeField] private float maxHealth = 100f;
     [SerializeField] private Weapon weapon;
 
-    private Rigidbody2D _rb;
+    private Rigidbody2D _rigidbody;
+    private PlayerHealth _playerHealth;
 
     private Vector2 _mousePosition;
     private Vector2 _movementInput;
@@ -19,20 +19,21 @@ public class PlayerController : MonoBehaviour, IUpgradeablePlayer {
     private Vector3 _aimDirection;
     private float _angle;
 
-    private float _currentHealth;
-
     private void Awake() {
         _playerInput = GetComponent<PlayerInput>();
-        _rb = GetComponent<Rigidbody2D>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _playerHealth = GetComponent<PlayerHealth>();
 
-        ResetHealth();
+        _playerHealth.ResetHealth();
+
+        UpgradeManager.Init(this);
     }
 
     void FixedUpdate() {
-        _rb.MovePosition(_rb.position + _movementInput * (moveSpeed * UpgradeManager.GetMovementSpeedMultiplier() * Time.fixedDeltaTime));
-        UpgradeManager.MovementUpdate(this);
+        _rigidbody.MovePosition(_rigidbody.position + _movementInput * (moveSpeed * UpgradeManager.GetMovementSpeedMultiplier() * Time.fixedDeltaTime));
+        UpgradeManager.PlayerUpdate(this);
     }
-    
+
     private void OnMove(InputValue value) {
         _movementInput = value.Get<Vector2>();
     }
@@ -46,34 +47,11 @@ public class PlayerController : MonoBehaviour, IUpgradeablePlayer {
         _aimDirection = value.Get<Vector2>();
         if (Vector2.Distance(Vector2.zero, _aimDirection) > 0.5) {
             if (_playerInput.currentControlScheme.Equals("Keyboard&Mouse")) {
-                _aimDirection = (Vector2)Camera.main.ScreenToWorldPoint(_aimDirection) - _rb.position;
+                _aimDirection = (Vector2)Camera.main.ScreenToWorldPoint(_aimDirection) - _rigidbody.position;
             }
 
             _angle = Mathf.Atan2(_aimDirection.y, _aimDirection.x) * Mathf.Rad2Deg - 90;
-            _rb.rotation = _angle;
-            Debug.Log(_angle);
-        }
-    }
-
-    /// <summary>
-    /// Resets the player's health. The currently active upgrades are taken into account.
-    /// </summary>
-    public void ResetHealth() {
-        _currentHealth = maxHealth * UpgradeManager.GetHealthMultiplier();
-    }
-
-    /// <summary>
-    /// Decreases the player's health by the specified value and checks if the player dies. If so, affecting upgrades are performed and further actions are initiated.
-    /// </summary>
-    /// <param name="damageAmount">Amount of damage the player should take</param>
-    public void TakeDamage(float damageAmount) {
-        _currentHealth -= damageAmount;
-
-        if (_currentHealth <= 0) {
-            UpgradeManager.OnPlayerDeath(this);
-            if (_currentHealth <= 0) {
-                // further actions
-            }
+            _rigidbody.rotation = _angle;
         }
     }
 
@@ -95,7 +73,7 @@ public class PlayerController : MonoBehaviour, IUpgradeablePlayer {
     }
 
     public void ExecutePhoenix_OnPlayerDeath() {
-        ResetHealth();
+        _playerHealth.ResetHealth();
     }
 
     #endregion
--- Assets/Characters/AimPlayer/Weapon.cs
+++ Assets/Characters/AimPlayer/Weapon.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour {
     private const int DefaultBulletCount = 1;
-    
+    private const float DefaultBulletSpreadAngle = 2f;
+
     [SerializeField] private Transform bulletPrefab;
     [SerializeField] private Transform firePoint;
 
     [SerializeField] private float fireForce = 20f;
 
     public void Fire() {
-        
         int bulletCount = DefaultBulletCount + UpgradeManager.GetBulletCountAdjustment();
-        
+
         for (int i = 0; i < bulletCount; i++) {
             Transform bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-            bullet.localRotation *= Quaternion.Euler(i * 10, i * 10, i * 10);
-            bullet.GetComponent<Rigidbody2D>().velocity = (Vector2) firePoint.up * fireForce;
+            //TODO: Correct parameters 
+            bullet.GetComponent<Bullet>().Init(0, transform.parent.gameObject);
+            bullet.GetComponent<Rigidbody2D>().velocity = (Vector2)(Quaternion.Euler(0, 0, (i - (bulletCount - 1) / 2.0f) * DefaultBulletSpreadAngle) * firePoint.up) * fireForce;
         }
-        
     }
 }
