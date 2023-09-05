30cb1c89c54ce01a43291b6cc29b7cc715de4512
Hans (cgt103529) <cgt103529@stud.fh-wedel.de>
2023-09-05 22:27:31 +0200

    Tilemaps broken and repaired and some level changes

--- Assets/Content/UI/Scripts/UIManager.cs
+++ Assets/Content/UI/Scripts/UIManager.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class UIManager : MonoBehaviour
 {
     [SerializeField] private GameObject deathPanel;
+    [SerializeField] private GameObject pausePanel;
 
     public void ToggleDeathPanel()
     {
         deathPanel.SetActive(!deathPanel.activeSelf);
+    }
+    
+    public void TogglePausePanel()
+    {
+        pausePanel.SetActive(!pausePanel.activeSelf);
     }
 }
