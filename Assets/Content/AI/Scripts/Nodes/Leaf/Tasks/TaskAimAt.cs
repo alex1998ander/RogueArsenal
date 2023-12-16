59d5e72745bf992baefe59c4c6b749df90806c24
alex1998ander <alexander.fedorov11@gmail.com>
2023-12-16 11:42:53 +0100

    Fixed TurretEnemy sprite misalignment

--- Assets/Content/AI/Scripts/Nodes/Leaf/Tasks/TaskAimAt.cs
+++ Assets/Content/AI/Scripts/Nodes/Leaf/Tasks/TaskAimAt.cs
@@ -9,21 +9,24 @@ namespace BehaviorTree
     public class TaskAimAt : Node
     {
         // Rigidbody of the enemy
-        private Rigidbody2D _rb;
+        private readonly Rigidbody2D _rb;
 
         // Weapon of enemy
-        private EnemyWeapon _weapon;
+        private readonly EnemyWeapon _weapon;
 
         // Transform of the player
-        private Transform _aimAtTransform;
+        private readonly Transform _aimAtTransform;
 
-        private SpriteRenderer _weaponSprite;
+        private readonly SpriteRenderer _weaponSprite;
 
-        public TaskAimAt(Rigidbody2D rb, EnemyWeapon weapon, Transform aimAtTransform)
+        private readonly bool _adjustSprite;
+
+        public TaskAimAt(Rigidbody2D rb, EnemyWeapon weapon, Transform aimAtTransform, bool adjustSprite = true)
         {
             _rb = rb;
             _weapon = weapon;
             _aimAtTransform = aimAtTransform;
+            _adjustSprite = adjustSprite;
 
             _weaponSprite = weapon.GetComponentInChildren<SpriteRenderer>();
         }
@@ -38,10 +41,13 @@ namespace BehaviorTree
                 float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
                 _weapon.transform.rotation = Quaternion.Euler(0, 0, angle);
 
-                // When the enemy is aiming left, flip weapon so it's not heads-down
-                _weaponSprite.flipY = aimDirection.x < 0.0f;
-                // When the enemy is aiming up, adjust sorting order so weapon is behind enemy
-                _weaponSprite.sortingOrder = angle >= 45.0 && angle <= 135.0f ? -1 : 1;
+                if (_adjustSprite)
+                {
+                    // When the enemy is aiming left, flip weapon so it's not heads-down
+                    _weaponSprite.flipY = aimDirection.x < 0.0f;
+                    // When the enemy is aiming up, adjust sorting order so weapon is behind enemy
+                    _weaponSprite.sortingOrder = angle >= 45.0 && angle <= 135.0f ? -1 : 1;
+                }
             }
 
             state = NodeState.SUCCESS;
