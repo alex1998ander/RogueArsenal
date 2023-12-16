26db50e4cdb1a8737e2ab174da1b11b2e68c9fe1
Hans (cgt103529) <cgt103529@stud.fh-wedel.de>
2023-12-16 12:27:59 +0100

    Levels filled even more + Lobby

---
+++ Assets/Content/Core/Scripts/DoorBehaviour.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorBehaviour : MonoBehaviour,ICharacterHealth
+{
+    [SerializeField] private float maxHealth = 1f;
+    private float _currentHealth;
+
+    private void Awake()
+    {
+        _currentHealth = maxHealth;
+    }
+
+    /// <summary>
+    /// Decreases the enemy's health by the specified value, checks if the enemy dies and triggers corresponding events. 
+    /// </summary>
+    /// <param name="damageAmount">Amount of damage</param>
+    /// <param name="fatal">ignored</param>
+    public void InflictDamage(float damageAmount, bool fatal = false, bool ignoreInvulnerability = false)
+    {
+        _currentHealth -= damageAmount;
+
+        //EventManager.OnEnemyDamage.Trigger(damageAmount);
+
+        // if enemy dies
+        if (_currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Content/Core/Scripts/TutorialTurret.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Content.Core.Scripts
+{
+    public class TutorialTurret: MonoBehaviour
+    {
+        [SerializeField] EnemyWeapon weapon;
+        private readonly float _weaponCooldown = 0.15f;
+        private void Start()
+        {
+            StartCoroutine(Shoot());
+        }
+
+        IEnumerator Shoot()
+        {
+            yield return new WaitForSeconds(_weaponCooldown);
+            weapon.Fire();
+            EventManager.OnEnemyShotFired.Trigger();
+            StartCoroutine(Shoot());
+        }
+    }
+}
