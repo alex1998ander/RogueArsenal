f4814565e7fc1f641c076a58bdfabd1463d857e0
Hans (cgt103529) <cgt103529@stud.fh-wedel.de>
2023-12-16 12:55:05 +0100

    Fixed NPC

--- Assets/Content/AI/Scripts/Trees/Enemies/MovingEnemies/Variants/NPCBehaviourTree.cs
+++ Assets/Content/AI/Scripts/Trees/Enemies/MovingEnemies/Variants/NPCBehaviourTree.cs
@@ -4,18 +4,19 @@ using UnityEngine.AI;
 
 namespace BehaviorTree
 {
-    public class NPCBehaviourTree: MovingEnemyBehaviourTree
+    public class NPCBehaviourTree : MovingEnemyBehaviourTree
     {
         [SerializeField] GameObject exitPoint;
+
         protected override Node SetupTree()
         {
             base.SetupTree();
-            
+
             Rigidbody2D rb = GetComponent<Rigidbody2D>();
             NavMeshAgent agent = GetComponent<NavMeshAgent>();
             agent.updateRotation = false;
             agent.updateUpAxis = false;
-            
+
             SharedData sharedData = new SharedData();
             Node root = new Selector(new List<Node>
             {
@@ -25,7 +26,7 @@ namespace BehaviorTree
                     new TaskLookAtMovementDirection(rb),
                     new TaskPickTargetAroundTransforms(transform, minDistanceFromPlayer,
                         maxDistanceFromPlayer),
-                    new TaskMoveToTarget(rb, agent, 1f),
+                    new TaskMoveToTarget(rb, agent, null, 1f),
                 }),
                 new Sequence(new List<Node>
                 {
@@ -33,7 +34,7 @@ namespace BehaviorTree
                     new SetData<bool>(sharedData.IsAwareOfPlayer, true),
                     new TaskPickTargetAroundTransforms(exitPoint.transform, minDistanceFromPlayer,
                         maxDistanceFromPlayer),
-                    new TaskMoveToTarget(rb, agent, 1f),
+                    new TaskMoveToTarget(rb, agent, null, 1f),
                     new CheckIsAtTarget(),
                     new TaskDeleteGameObject(transform)
                 }),
@@ -42,6 +43,5 @@ namespace BehaviorTree
             root.SetupSharedData(sharedData);
             return root;
         }
-        
     }
 }
