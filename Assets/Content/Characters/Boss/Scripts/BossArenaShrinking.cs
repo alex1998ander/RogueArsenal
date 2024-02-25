9eb9fcfd5bf466469d5c6a4c0936ee0d2c86ddf2
Hans (cgt103529) <cgt103529@stud.fh-wedel.de>
2024-02-25 21:24:38 +0100

    Boss changes + boss arena shrinking + background

--- Assets/Content/Characters/Boss/Scripts/BossArenaShrinking.cs
+++ Assets/Content/Characters/Boss/Scripts/BossArenaShrinking.cs
@@ -6,16 +6,54 @@ public class BossArenaShrinking : MonoBehaviour
     [SerializeField] private Transform[] walls;
     private EnemyHealth _bossHealth;
 
+    private Vector3 [] _startMarker = new Vector3[3];
+    private Vector3 [] _endMarker = new Vector3[3];
+
+    // Movement speed in units per second.
+    public float speed = 0.1F;
+
+    // Time when the movement started.
+    private float _startTime;
+
+    // Total distance between the markers.
+    private float  _journeyLength;
+
+    private Vector3 _direction;
+    
     private void Start()
     {
         _bossHealth = FindObjectOfType<EnemyHealth>();
+        
+        for (int i = 0; i < 3; i++)
+        {
+            Debug.Log(i);
+            Debug.Log(_startMarker[i]);
+            Debug.Log(walls[i].transform.position);
+            _startMarker[i] = walls[i].transform.position;
+            
+            _direction = Vector3.Normalize(transform.position - walls[i].position);
+            _endMarker[i] = _startMarker[i] + _direction * 5;
+        }
+        
+        // Calculate the journey length.
+        _journeyLength = Vector3.Distance(_startMarker[0], _endMarker[0]);
     }
 
     private void Update()
     {
-        // if (_bossHealth.GetHealth().x * 2 / 3 < _bossHealth.GetHealth().y)
-        // {
-        //     transform.position = Vector3.Lerp(walls[0].transform.position, transform.position, 1f);
-        // }
+        if (_bossHealth.GetHealth().x * 2 / 3 < _bossHealth.GetHealth().y)
+        {
+            _startTime = _startTime== 0? Time.time : _startTime;
+            // Distance moved equals elapsed time times speed..
+            float distCovered = (Time.time - _startTime) * speed;
+
+            // Fraction of journey completed equals current distance divided by total distance.
+            float fractionOfJourney = distCovered / _journeyLength;
+            for (int i = 0; i < 3; i++)
+            {
+                walls[i].transform.position = Vector3.Lerp(_startMarker[i], _endMarker[i], fractionOfJourney);
+            }
+             
+        }
     }
 }
