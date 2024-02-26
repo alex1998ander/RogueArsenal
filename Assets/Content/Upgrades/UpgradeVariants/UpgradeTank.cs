fcfafb72a7021114669f7b4061120d765541e478
alex1998ander <alexander.fedorov11@gmail.com>
2024-02-27 00:29:13 +0100

    tank to passive

--- Assets/Content/Upgrades/UpgradeVariants/UpgradeTank.cs
+++ Assets/Content/Upgrades/UpgradeVariants/UpgradeTank.cs
@@ -2,7 +2,7 @@
 {
     public override string Name => "Tank";
     public override UpgradeIdentification UpgradeIdentification => UpgradeIdentification.Tank;
-    public override UpgradeType UpgradeType => UpgradeType.Weapon;
+    public override UpgradeType UpgradeType => UpgradeType.Passive;
     public override string FlavorText => "Roar into battle as the ferocious Tankasaurus, impervious to damage and ready to stomp through enemy lines.";
     public override string Description => "Roar into battle as the ferocious Tankasaurus.";
 
